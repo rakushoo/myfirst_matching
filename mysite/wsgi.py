26f7b8eac1f828c1aaaa548e56aa5f887b3b4598
Shogo Kohraku <skohraku@gmail.com>
2020-04-30 22:08:36 +0900

    add simple html

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -15,5 +15,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+# localで確認するときはコメントアウトにする
+#from whitenoise.django import DjangoWhiteNoise
+#application = DjangoWhiteNoise(application)
